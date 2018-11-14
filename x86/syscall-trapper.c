b386c9707970535e6e58a78604ff2b92d3c2f10e
sandwichdoge <sandwichdoge@gmail.com>
2018-11-15 01:34:56 +0700

    handle more syscalls

--- x86/syscall-trapper.c
+++ x86/syscall-trapper.c
@@ -7,18 +7,23 @@
 #include <sys/ptrace.h>
 #include <sys/user.h>
 
+#define MAX_CHAR_PEEK 1024
 
 //Just some example syscalls
 //http://blog.rchapman.org/posts/Linux_System_Call_Table_for_x86_64/
 enum syscalls {SYS_READ = 0, SYS_WRITE, SYS_OPEN, SYS_CLOSE, SYS_STAT,
                         SYS_FSTAT, SYS_LSTAT, SYS_POLL, SYS_LSEEK, SYS_MMAP,
-                        SYS_MPROTECT, SYS_MUNMAP, SYS_BRK, SYS_ACCESS = 21,
+                        SYS_MPROTECT, SYS_MUNMAP, SYS_BRK, SYS_IOCTL = 16,
+                        SYS_ACCESS = 21,
+                        SYS_DUP2 = 33,
+                        SYS_FCNTL = 72,
                         SYS_CREAT = 85, SYS_OPENAT = 257};
 
 
 int print_syscall_info(pid_t pid, struct user_regs_struct *regs);
 int peek_str(pid_t pid, unsigned long long addr);
 
+int PRINT_BUFFER = 0; //print read/written data
 
 int main(int argc, char **argv)
 {
@@ -30,11 +35,11 @@ int main(int argc, char **argv)
         case -1:
                 perror("fork() error.\n");
                 return -1;
-        case 0: //child
+        case 0: //child proc
                 if (ptrace(PTRACE_TRACEME, pid, 0, 0) < 0) perror("FATAL: ptrace from child"); //this will pause child
                 if (execvp(cmd[0], cmd) < 0) perror("exec error");
                 break;
-        default:
+        default: //parent
                 waitpid(pid, 0, 0); //sync with TRACEME from child
                 ptrace(PTRACE_SETOPTIONS, pid, 0, PTRACE_O_EXITKILL); //child process should terminate with parent
 
@@ -44,11 +49,11 @@ int main(int argc, char **argv)
                                 break;
                         }
                         waitpid(pid, &status, 0); //wait until child process is ready
+                        //[WE CAN INTERCEPT ARGUMENT VALUES HERE]
 
                         struct user_regs_struct regs;
 
                         ptrace(PTRACE_GETREGS, pid, 0, &regs);
-                        //printf("Got syscall: %ld\n", regs.orig_rax);
                         print_syscall_info(pid, &regs);
 
                         /*Execute syscall and stop right before return*/
@@ -57,7 +62,8 @@ int main(int argc, char **argv)
 
                         ptrace(PTRACE_GETREGS, pid, 0, &regs);
                         //printf("Got retval: %d\n\n", regs.rax);
-
+                        //[WE CAN INTERCEPT RETURN VALUE HERE]
+                        
                         if (WIFEXITED(status)) break; //child exited
                 }
         }
@@ -68,15 +74,14 @@ int main(int argc, char **argv)
 
 int print_syscall_info(pid_t pid, struct user_regs_struct *regs)
 {
-        char *fname;
-        int i = 0;
-
         switch (regs->orig_rax) {
         case SYS_READ:
-                printf("READ syscall on fd:%d", regs->rdi);
+                printf("READ %d bytes on fd:%d", regs->rdx, regs->rdi);
+                if (PRINT_BUFFER) peek_str(pid, regs->rsi);
                 break;
         case SYS_WRITE:
-                printf("WRITE syscall on fd:%d", regs->rdi);
+                printf("WRITE %d bytes on fd:%d", regs->rdx, regs->rdi);
+                if (PRINT_BUFFER) peek_str(pid, regs->rsi);
                 break;
         case SYS_OPEN:
                 printf("OPEN syscall on file:");
@@ -89,9 +94,33 @@ int print_syscall_info(pid_t pid, struct user_regs_struct *regs)
                 printf("STAT syscall on file:");
                 peek_str(pid, regs->rdi);
                 break;
-        case SYS_ACCESS:
+        case SYS_FSTAT:
+                printf("FSTAT syscall on fd:", regs->rdi);
+                break;
+        case SYS_LSEEK:
+                printf("LSEEK syscall on fd: %d", regs->rdi);
+                break;
+        case SYS_MMAP:
+                printf("MMAP syscall at %ld", regs->rdi);
+                break;
+        case SYS_MUNMAP:
+                printf("MUNMAP syscall at %ld", regs->rdi);
+                break;
+        case SYS_BRK:
+                printf("BRK syscall");
+                break;
+        case SYS_IOCTL:
+                printf("IOCTL syscall on fd: %d, cmd: %d, arg: %ld", regs->rdi, regs->rsi, regs->rdx);
+                break;
+        case SYS_ACCESS: //21
                 printf("ACCESS syscall on file:");
                 peek_str(pid, regs->rdi);
+                break;
+        case SYS_DUP2: //33
+                printf("DUP2 syscall, old fd: %d, new fd: %d", regs->rdi, regs->rsi);
+                break;
+        case SYS_FCNTL: //72
+                printf("IOCTL syscall on fd: %d, cmd: %d, arg: %ld", regs->rdi, regs->rsi, regs->rdx);
                 break;
         case SYS_CREAT:
                 printf("CREAT syscall on file:");
@@ -127,6 +156,7 @@ int peek_str(pid_t pid, unsigned long long addr)
                         if (word.c[j] == '\0') return 0;
                         printf("%c", word.c[j]);
                 }
+                if (i > MAX_CHAR_PEEK) break;
         }
 
         return 0;
